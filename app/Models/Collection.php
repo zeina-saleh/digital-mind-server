6a3f02276addcaac3525f929ed06e16caa98fb1a
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-11 08:56:19 +0300

    created relations

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Idea;
 
 class Collection extends Model
 {
@@ -12,4 +14,9 @@ class Collection extends Model
         'user_id',
         'title'
     ];
+
+    public function ideas(): HasMany
+    {
+        return $this->hasMany(Idea::class);
+    }
 }
--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Like;
 
 class Idea extends Model
 {
@@ -12,4 +14,8 @@ class Idea extends Model
         'resource_id',
         'collection_id'
     ];
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
 }
