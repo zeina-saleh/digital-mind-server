673f0ba6ef56d178ac343ac3cb3a38a529fede30
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-11 09:59:39 +0300

    implemented get user collections function

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Collection;
+use App\Models\Idea;
 
 class MapController extends Controller
 {
-    //
+    public function getUserCollections()
+    {
+        $user = Auth::user();
+        $user_id = $user->id;
+        $collections = Collection::with(['ideas'])->where('user_id', $user_id)->get();
+        return response()->json($collections);
+    }
+
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MapController;
 
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
 
 Route::group(["middleware" => "auth:api"], function () {
+    Route::get('/getUserCollections', [MapController::class, 'getUserCollections']);
+    //Route::get('/getIdeas', [MapController::class, 'getIdeas']);
 });
