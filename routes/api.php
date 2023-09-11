109638028e7921b83dc45f1a4b6b9cb9f0c609f9
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-11 07:50:51 +0300

    implemented basic route setup

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\AuthController;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+Route::post("/login", [AuthController::class, "login"]);
+Route::post("/register", [AuthController::class, "register"]);
+
+Route::group(["middleware" => "auth:api"], function () {
 });
