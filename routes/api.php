c7252d1f795022e72374bdea4a6d937e90ea585a
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-11 07:21:26 +0300

    initializing laravel project + jwt config

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
