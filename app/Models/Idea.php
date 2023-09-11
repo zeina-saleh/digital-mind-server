7afe175ecd1830a180c16211903cb8a49762bfff
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-11 07:50:25 +0300

    implemented basic db setup

---
+++ app/Models/Idea.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Idea extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'resource_id',
+        'collection_id'
+    ];
+}
