c7aeda3faba71335dad9cbc8b8fe7ae15ffdae23
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-13 10:57:50 +0300

    implemented like function

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -6,20 +6,51 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Collection;
 use App\Models\Idea;
+use App\Models\Like;
 
 class MapController extends Controller
 {
     public function getUserCollections()
     {
         $user = Auth::user();
-        $user_id = $user->id;
-        $collections = Collection::with(['ideas'])->where('user_id', $user_id)->get();
+
+        $collections = Collection::with(['ideas'])->where('user_id', $user->id)->get();
         return response()->json($collections);
     }
 
     public function getIdeas()
     {
+        $user = Auth::user();
+
         $ideas = Idea::withCount('likes')->with(['collection.user'])->get();
+        foreach ($ideas as $idea) {
+        $existingLike = Like::where('user_id', $user->id)->where('idea_id', $idea->id)->first();
+
+        $idea->liked = !is_null($existingLike);
+    }
         return response()->json($ideas);
+    }
+
+    public function likeIdea(Request $request, $ideaId)
+    {
+        $user = Auth::user();
+        $idea = Idea::findOrFail($ideaId);
+
+        $existingLike = Like::where('user_id', $user->id)->where('idea_id', $idea->id)->first();
+
+        if ($existingLike) {
+            $existingLike->delete();
+            $idea = Idea::withCount('likes')->where('id', $ideaId)->get();
+            return response()->json(['idea' => $idea]);
+        }
+
+        $like = new Like();
+        $like->user_id = $user->id;
+        $like->idea_id = $idea->id;
+        $like->save();
+
+        $idea = Idea::withCount('likes')->where('id', $ideaId)->get();
+
+        return response()->json(['idea' => $idea]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,5 @@ Route::post("/register", [AuthController::class, "register"]);
 Route::group(["middleware" => "auth:api"], function () {
     Route::get('/getUserCollections', [MapController::class, 'getUserCollections']);
     Route::get('/getIdeas', [MapController::class, 'getIdeas']);
+    Route::post('/likeIdea/{ideaId}', [MapController::class, 'likeIdea']);
 });
