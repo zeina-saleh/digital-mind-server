bbe910157bd95d0498e2a0429e06d4456d16c018
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-14 17:48:10 +0300

    added foreign keys references

---
+++ database/migrations/2023_09_14_144308_modify_tags_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->foreign('idea_id')->references('id')->on('ideas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
