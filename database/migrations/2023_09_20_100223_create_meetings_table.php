3377913960b4ca55ea76ed4b4c9def0bb4198a82
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-20 15:20:36 +0300

    added meetings table

---
+++ database/migrations/2023_09_20_100223_create_meetings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idea_id');
+            $table->string('title');
+            $table->date('datetime');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+            $table->foreign('idea_id')->references('id')->on('ideas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meetings');
+    }
+};
