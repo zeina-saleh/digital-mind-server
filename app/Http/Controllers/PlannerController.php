cb97ac83989e3bacd9099ad1655c3e5f7dc98fb1
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-21 17:49:56 +0300

    created planner controller

---
+++ app/Http/Controllers/PlannerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Meeting;
+use Illuminate\Support\Facades\Auth;
+
+class PlannerController extends Controller
+{
+    function createMeeting(Request $request, $ideaId)
+    {
+        $user = Auth::user();
+        try {
+            $validatedData = $request->validate([
+                'title' => 'required|string',
+                'date' => 'required|string',
+                'latitude' => 'required|numeric',
+                'longitude' => 'required|numeric',
+            ]);
+            $meeting = new Meeting();
+            $meeting->idea_id = $ideaId;
+            $meeting->user_id = $user->id;
+            $meeting->title = $validatedData['title'];
+            $meeting->datetime = $validatedData['date'];
+            $meeting->latitude = $validatedData['latitude'];
+            $meeting->longitude = $validatedData['longitude'];
+            $meeting->save();
+            return response()->json(['message' => 'Meeting added successfully', 'meeting' => $meeting], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    function getDate()
+    {
+        $user_id = Auth::user()->id;
+
+        $plans = Meeting::where('user_id', $user_id)->get();
+        $schedule = $plans->map(function ($plan) {
+            return [
+                "date" => $plan->datetime,
+                "title" => $plan->title,
+            ];
+        });
+
+        return response()->json(["schedule" => $schedule]);
+    }
+}
