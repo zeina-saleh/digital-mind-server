64b3da82c0f1669c306457ab14a014c9628b526b
zeinaCodes <eye_likemoon@hotmail.com>
2023-09-23 00:29:27 +0300

    Implemented send message function

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Discussion;
+use App\Models\User;
+use App\Models\Message;
 
 class ChatsController extends Controller
 {
@@ -22,6 +24,7 @@ class ChatsController extends Controller
             $discussion->title = $validatedData['title'];
             $discussion->save();
 
+            $discussion->users()->attach([$user->id]);
             $discussion->users()->attach($validatedData['members']);
 
             return response()->json(['message' => 'Discussion created successfully'], 200);
@@ -29,4 +32,47 @@ class ChatsController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
+    public function getUserDiscussions()
+    {
+        $user = Auth::user();
+        $discussions = User::find($user->id)->discussions()->with('idea', 'users')->get();
+
+        return response()->json([
+            "discussions" => $discussions,
+            "authUser" => $user->name
+        ]);
+    }
+
+    public function sendMessage(Request $request, $discussionId)
+    {
+        $user = Auth::user();
+
+        try {
+            $validatedData = $request->validate([
+                'text' => 'required|string',
+            ]);
+
+            $message = new Message();
+            $message->discussion_id = $discussionId;
+            $message->user_id = $user->id;
+            $message->text = $validatedData['text'];
+            $message->save();
+
+            return response()->json($message);
+        } catch (\Throwable $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /*public function getMessages($discussionId)
+    {
+        $user = Auth::user();
+        $messages = Message::find($discussionId)->with('user')->get();
+
+        return response()->json([
+            "messages" => $messages,
+            "authUser" => $user->id
+        ]);
+    }*/
 }
